68c6d3f6c805e0ba2f9a967cdc0567548428ba1a
Nacer Bouchekhima <nbouchek@gmail.com>
2019-09-30 16:54:56 +0100

    Demoing bean lifecycle.

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -20,4 +20,14 @@ public class TrackCoach implements Coach {
     public String getDailyFortune() {
         return "Just do it! " + fortuneService.getFortune();
     }
+
+    // add an init methdd
+    public void doMyStartupStuff() {
+        System.out.println("TrackCoach: inside doMyStuff");
+    }
+
+    //  add a destroy method
+    public void doMyCleanupStuff() {
+        System.out.println("TrackCoack: inside method doMyCleanupStuff");
+    }
 }
